414b4a663765523a9b872fa0a158265f7ff0476f
Mike <m.boroznjak@gmail.com>
2020-12-08 12:48:07 +0200

    Âåðñèÿ 1.1

--- main.cpp
+++ main.cpp
@@ -3,20 +3,7 @@
 
 using namespace std;
 
-int main(){
-int f(int p)
-{
-    return 100;
-};
-
-template <typename T>
-int f(T p)
-{
-    return 200;
-}
-
 int main()
 {
-    printf("%d", f(42));
-}
+    printf("Test.\n");
 }
